8ae6741ca6087fc9b67f57d28a525c4c8357bba5
garelickja <138186599+garelickja@users.noreply.github.com>
2024-03-11 15:45:11 -0500

    target sum

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (i !== j && array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
-
 /* 
   Write the Big O time complexity of your function here
+  BigO = (n^2)
 */
 
 /* 
   Add your pseudocode here
+  - loop through the array
+  - loop through the array again
+  - check if the sum of the two numbers is equal to the target
+  - if the sum equals the target, return true
+  - If the loop finishes, return false
 */
 
 /*
   Add written explanation of your solution here
+  - I used a nested loop to iterate through the array and check if the sum of two numbers is equal to the target
+  - If the sum is equal to the target, then return true
+  - If the loops finish without finding a match, I return false
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +45,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 19);
 }
 
 module.exports = hasTargetSum;
+
+
